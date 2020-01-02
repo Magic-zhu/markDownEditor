08b7caefef3e6140e9917ede1af648702fed9c02
alex.zhu <2489180202@qq.com>
2020-01-02 11:08:47 +0800

    Auto stash before merge of "develop" and "origin/develop"

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const { watch, series } = require('gulp');
+function defaultTask(cb) {
+    // place code for your default task here
+    cb();
+  }
+  function restart() {
+      
+  }
+  watch("body/*",restart)
+  exports.default = defaultTask
