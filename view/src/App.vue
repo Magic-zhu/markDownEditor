e2829cfb14b6adb608d290850976a45c9faff9f7
magic-zhu <18817313320@163.com>
2020-06-26 18:10:57 +0800

    save

--- view/src/App.vue
+++ view/src/App.vue
@@ -5,8 +5,16 @@
 </template>
 
 <style lang="less">
+    body,html{
+        width: 100%;
+        height: 100%;
+    }
     body,html,div,span,p{
         margin: 0;
         padding: 0;
+    }
+    #app{
+        width: 100%;
+        height: 100%;
     }
 </style>
